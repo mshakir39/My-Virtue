ac12786e6c0973565d3ce75010003fe04a2f259d
Muzamil Shakir <mshakir39@gmail.com>
2023-11-09 21:10:24 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    MONGO_URI: "mongodb+srv://Muzamil:3412123@cluster0.xz9ul.mongodb.net/My_VirtueDB?retryWrites=true&w=majority"
+    MONGO_URI: ""
   },
   reactStrictMode: true,
 }
